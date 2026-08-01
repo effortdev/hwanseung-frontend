102def458e8ab481f85608fe42b2bdbc23e42cde
ktj <kindxowns@gmail.com>
2026-08-01 21:12:29 +0900

    oauth수정

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import './index.css'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const GOOGLE_CLIENT_ID = "970476509867-7l2au3i2f5qhsv69bi7hm7cicbbeebl5.apps.googleusercontent.com";
+// const GOOGLE_CLIENT_ID = "970476509867-7l2au3i2f5qhsv69bi7hm7cicbbeebl5.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "283175965771-j22rti4go8lilp5nlaa5dk5v4i2c2vca.apps.googleusercontent.com";
 
 createRoot(document.getElementById('root')).render(
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
